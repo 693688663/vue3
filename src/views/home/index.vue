df7f9f07de1a636f164aa67fafb8d9b49b3434e5
dingding <693688663@qq.com>
2022-06-29 18:01:38 +0800

    头部tab与路由联动

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,6 +1,6 @@
 <!--
  * @LastEditors: 丁玉欣
- * @LastEditTime: 2022-06-29 10:01:52
+ * @LastEditTime: 2022-06-29 18:00:38
  * @Description: 主页
 -->
 <template>
@@ -21,6 +21,13 @@
       <a-layout>
         <a-layout-header theme="light">Header</a-layout-header>
         <a-layout-content theme="light">
+          <div>
+            <a-tabs type="editable-card" hide-add @edit="fun.onEdit" @change="fun.tabChange" :activeKey="activeKey">
+              <a-tab-pane v-for="pane in panes" :key="pane.key" :tab="pane.title">
+                <!-- {{ pane.content }} -->
+              </a-tab-pane>
+            </a-tabs>
+          </div>
           <!-- <keep-alive> -->
           <router-view> </router-view>
           <!-- </keep-alive> -->
@@ -42,8 +49,7 @@ import { menuList } from "@/router/home/allMenu"
 import { useRouter, useRoute } from "vue-router";
 const router = useRouter()
 const route: any = useRoute()
-// console.log(route.fullPath)
-// console.log(list)
+
 // 数据
 // 是否收起左侧菜单 
 const collapsed = ref<Boolean>(false)
@@ -51,16 +57,24 @@ const collapsed = ref<Boolean>(false)
 const openKeys = ref<String[]>([])
 // 选中的菜单
 const selectedKeys = ref<String[]>([])
+// 横向菜单列表
+const panes = ref<any>([])
+// 当前菜单
+const activeKey = ref('')
+
 //  函数
 const fun = {
-  // 菜单选择时展示不同菜单
+  // 菜单选择：展示不同的菜单
   clickMenuItem: ({ item, key, keyPath }) => {
     openKeys.value = keyPath
     selectedKeys.value = keyPath
   },
-  // 初始时设置展开的菜单
+  // 监听路由变化
   setKeyPath: () => {
+
+    // 设置左侧菜单数据
     let routeKey = route.meta.key || ""
+    if (!routeKey) return;
     let list = routeKey.split('-')
     let keyPath: String[] = []
     for (let i = 0; i < list.length; i++) {
@@ -72,8 +86,80 @@ const fun = {
     }
     openKeys.value = keyPath
     selectedKeys.value = keyPath
+
+    // 设置头部菜单数据
+    if (panes.value.length == 0) {
+      panes.value.push({
+        path: route.fullPath,
+        title: route.meta.title,
+        key: route.meta.key
+      })
+    }
+    for (let i = 0; i < panes.value.length; i++) {
+      let item = panes.value[i]
+      if (item.key == route.meta.key) {
+        break
+      }
+      if (i == panes.value.length - 1) {
+        panes.value.push({
+          path: route.fullPath,
+          title: route.meta.title,
+          key: route.meta.key
+        })
+      }
+    }
+
+    // 改变默认菜单
+    activeKey.value = route.meta.key
+
+  },
+
+  // 头部菜单关闭
+  onEdit: (targetKey) => {
+
+    // 有一个菜单时
+    // 清空头部菜单 跳转主页
+    if (panes.value.length == 1) {
+      panes.value = []
+      router.push('/home')
+      return
+    }
+
+    // 有多个菜单时
+    // 删除当前菜单
+    // 删除选中的菜单时则进入前一个菜单
+    // 删除未选中的菜单时则只删除菜单
+    let arr: any = []
+    let num = 0
+    for (let i = 0; i < panes.value.length; i++) {
+      let item = panes.value[i]
+      if (panes.value[i].key == targetKey) {
+        num = i
+        continue
+      }
+      arr.push(panes.value[i])
+    }
+    if (targetKey == route.meta.key) {
+      if (num == 0) {
+        router.push(panes.value[num + 1].path)
+      } else {
+        router.push(panes.value[num - 1].path)
+      }
+    }
+    panes.value = arr
+  },
+
+  // 头部菜单切换
+  tabChange: (activeKey) => {
+    for (let i = 0; i < panes.value.length; i++) {
+      if (panes.value[i].key == activeKey) {
+        router.push(panes.value[i].path)
+        break;
+      }
+    }
   }
 }
+
 // 监听路由变化
 watch(() => route.fullPath, () => {
   fun.setKeyPath()
